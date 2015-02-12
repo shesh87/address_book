579117533967fe5a27b4c9f3806704e61d8576b4
Sheonna Harris <svonharris@gmail.com>
2015-02-12 16:41:35 -0500

    Just figured out how to add JS to rails.

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
-gem 'jquery-rails'
+gem 'jquery-rails', '~> 4.0.3'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,18 +6,6 @@ class Contact < ActiveRecord::Base
 	validates :email_address_alt, allow_blank: true, uniqueness: true
 
 
-	def self.order_contacts
-		order(name: :asc)
-	end
-
-	# def self.order_contacts(direction)
-	# 	if direction == "asc"
-	# 		order(name: :asc)
-	# 	else
-	# 		order(name: :desc)
-	# 	end
-	# end
-
 	def self.sort_letter(letter)
 		where("name LIKE ?", "#{letter}%")
 	end
