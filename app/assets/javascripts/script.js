329345eb3a58cf11934bc373017cdf86f98d3dea
Sheonna Harris <svonharris@gmail.com>
2015-02-18 17:16:57 -0500

    Started adding styling to the first page; feels like something's missing. Would love to add image to each contact and then displaying them here.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,29 +2,6 @@
 //to the server to retrieve said letter query.
 
 
-// $('.ltrs').on('click', function() {
-// 	var letter = $(this).html();
-// 	console.log(letter);
-// 	$.post('/contacts/sort_letter/' + letter, function(data) {
-// 		console.log(data);
-// 		console.log(data[0].name);
-// 		var names = data[0].name;
-// 		replace(names);
-// 	});
-// });
-
-
-// function replace(name) {
-// 	// $('.people').text(name);
-// 	// document.getElementById('people').innerHTML = "hello";
-// 	$('.list-container .list-index').css('display', 'none');
-// 	// alert('hi');
-// 	$('.list-container').prepend('<div>'+name+'</div>');
-// }
-
-
-
-
 $('.ltrs').on('click', function(e) {
 	e.preventDefault(); //stops page from reloading
 	var letter = $(this).html();
@@ -32,10 +9,10 @@ $('.ltrs').on('click', function(e) {
 	  url: '/contacts/sort_letter/' + letter,
 	  type: "POST",
 	}).done(function( msg ) {
-		$('.list-index').empty();
-		$('.list-index').prepend("<ul class='chosen_ltr'></ul>");
+		$('ul.contacts').empty();
+		// $('.list-index').prepend("<ul class='chosen_ltr'></ul>");
 		msg.forEach(function(person) {
-		    $('.chosen_ltr').append("<li><a href='/contacts/"+person.id+"'>" +person.name+ "</a></li>");
+		    $('ul.contacts').append("<li><a href='/contacts/"+person.id+"'>" +person.name+ "</a></li>");
 		});
 
 	});
