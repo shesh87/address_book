da9dce7c97c63878b5e31c05ca862400be20af53
Sheonna Harris <svonharris@gmail.com>
2015-02-19 13:56:16 -0500

    Started stylizing address book.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,17 +2,17 @@
 //to the server to retrieve said letter query.
 
 
-$('.ltrs').on('click', function(e) {
+$('.js-ltrs').on('click', function(e) {
 	e.preventDefault(); //stops page from reloading
 	var letter = $(this).html();
 	$.ajax({
 	  url: '/contacts/sort_letter/' + letter,
 	  type: "POST",
 	}).done(function( msg ) {
-		$('ul.contacts').empty();
+		$('.js-contacts').empty();
 		// $('.list-index').prepend("<ul class='chosen_ltr'></ul>");
 		msg.forEach(function(person) {
-		    $('ul.contacts').append("<li><a href='/contacts/"+person.id+"'>" +person.name+ "</a></li>");
+		    $('.js-contacts').append("<li><a href='/contacts/"+person.id+"'>" +person.name+ "</a></li>");
 		});
 
 	});
