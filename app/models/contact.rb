94671e1ed75e2d3f9b75acbbca6f62df1bf3141b
Sheonna Harris <svonharris@gmail.com>
2017-03-25 18:07:56 -0400

    updated ruby gems and database

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ActiveRecord::Base
 	validates :name, presence: true
 	validates :phone_number, allow_blank: true, numericality: true, length: { minimum: 10 }
-	validates :phone_number, allow_blank: true, numericality: true, length: { minimum: 10 }
+	validates :phone_number_alt, allow_blank: true, numericality: true, length: { minimum: 10 }
 	validates :email_address, allow_blank: true, uniqueness: true
 	validates :email_address_alt, allow_blank: true, uniqueness: true
 
